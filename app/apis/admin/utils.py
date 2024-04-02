1236534c137b4292d8fc97870bfb40529241f9b5
P4b3l1t0 <59421032+p4b3l1t0@users.noreply.github.com>
2024-04-02 20:51:15 +0400

    adding whitelist protection to avoid OS Command Injections utils.py

--- app/apis/admin/utils.py
+++ app/apis/admin/utils.py
@@ -2,6 +2,10 @@ import subprocess
 
 
 def get_disk_usage(parameters: str):
+    allowed_chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-_/ "
+    if not all(c in allowed_chars for c in parameters):
+        raise ValueError("Invalid characters in parameters")
+        
     command = "df -h " + parameters
 
     try:
@@ -11,5 +15,4 @@ def get_disk_usage(parameters: str):
         usage = result.stdout.strip().decode()
     except:
         raise Exception("An unexpected error was observed")
-
     return usage
